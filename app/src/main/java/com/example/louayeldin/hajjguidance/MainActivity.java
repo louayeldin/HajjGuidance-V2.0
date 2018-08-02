217205a94e354198de05a6dc42008fb81e54738e
louayeldin <louayeldin@gmail.com>
2018-08-03 01:02:13 +0300

    Updated version of our application v2.0

--- app/src/main/java/com/example/louayeldin/hajjguidance/MainActivity.java
+++ app/src/main/java/com/example/louayeldin/hajjguidance/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.louayeldin.hajjguidance;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -17,6 +19,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, View.OnClickListener {
@@ -45,6 +48,20 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         // Add a marker in Makkah, Saudi Arabia,
         // and move the map's camera to the same location.
+        // Customizing the map style to be look different from the Default layout of Google Map
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.map_style));
+            if (!success) {
+                Log.e("MainAcivity", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e("MainActivity", "Can't find style. Error: ", e);
+        }
+
         LatLng makkah = new LatLng(	21.427, 39.826);
         googleMap.addMarker(new MarkerOptions().position(makkah)
                 .title("مكة المكرمة"));
